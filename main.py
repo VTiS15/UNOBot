6a7134555670f1baa2299d92697a97a177c6e0d9
Vincent T <vtis.tsangvincent15@gmail.com>
2022-02-24 17:39:52 +0800

    v1.4.0 Update

--- main.py
+++ main.py
@@ -5671,7 +5671,8 @@ async def leavegame(ctx):
 
                         if len(games[str(ctx.guild.id)]['players']) >= 2:
                             await asyncio.gather(*[asyncio.create_task(x.send(
-                                embed=discord.Embed(description=':warning: **' + ctx.author.name + '** left.'))) for x
+                                embed=discord.Embed(description=':warning: **' + ctx.author.name + '** left.',
+                                                    color=discord.Color.red()))) for x
                                 in ctx.channel.category.text_channels])
 
                             await ctx.channel.delete()
