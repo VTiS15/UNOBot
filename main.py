23325e92315c9d05a32f26ce1c116f6f4d00c90a
Vincent T <vtis.tsangvincent15@gmail.com>
2022-05-19 23:25:22 +0800

    v1.4.2 Update

--- main.py
+++ main.py
@@ -5208,7 +5208,7 @@ async def startgame(ctx, *, args: Option(str, 'Game settings you wish to apply',
 
                             start = Button(label='Start now!', style=discord.ButtonStyle.blurple, emoji='▶️')
                             async def start_callback(interaction):
-                                await asyncio.sleep(1.5)
+                                asyncio.create_task(asyncio.sleep(1.5))
 
                                 await interaction.response.defer()
 
