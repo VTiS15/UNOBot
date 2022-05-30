03d3cccd6473828807ffbdb5f130474a953d54b1
Vincent T <vtis.tsangvincent15@gmail.com>
2022-05-30 18:32:27 +0800

    v1.5.1 Update

--- main.py
+++ main.py
@@ -2160,12 +2160,12 @@ class Bot:
                     n += 1
 
                 prob = 0
-                if n * least <= 12 * (total % 108 + 1):
+                if n * least <= 12 * (len(cards) % 108 + 1):
                     for i in range(n * least):
                         if i <= total:
-                            prob += comb(12 * (total % 108 + 1), i) * comb(96 * (total % 108 + 1) - len(self.cards),
+                            prob += comb(12 * (total // 108 + 1) - least, i) * comb(96 * (total // 108 + 1) - len(self.cards),
                                                                            total - i) / comb(
-                                108 * (total % 108 + 1) - len(self.cards), total)
+                                108 * (total // 108 + 1) - len(self.cards), total)
 
                 if prob > 0.3:
                     score = 10 * prob
@@ -2192,9 +2192,9 @@ class Bot:
                 if n * least <= 12 * (total % 112 + 1):
                     for i in range(n * least):
                         if i <= total:
-                            prob += comb(12 * (total % 112 + 1), i) * comb(100 * (total % 112 + 1) - len(self.cards),
+                            prob += comb(12 * (total // 112 + 1) - least, i) * comb(100 * (total // 112 + 1) - len(self.cards),
                                                                            total - i) / comb(
-                                112 * (total % 112 + 1) - len(self.cards), total)
+                                112 * (total // 112 + 1) - len(self.cards), total)
 
                 if prob > 0.3:
                     score = 10 * prob
@@ -2244,9 +2244,9 @@ class Bot:
                 prob = 0
                 if n * least <= 8 * (total % 112 + 1):
                     for i in range(n * least):
-                        prob += comb(8 * (total % 112 + 1), i) * comb(104 * (total % 112 + 1) - len(self.cards),
+                        prob += comb(8 * (total // 112 + 1) - least, i) * comb(104 * (total // 112 + 1) - len(self.cards),
                                                                       total - i) / comb(
-                            112 * (total % 112 + 1) - len(self.cards), total)
+                            112 * (total // 112 + 1) - len(self.cards), total)
 
                 if prob > 0.3:
                     score = 10 * prob
