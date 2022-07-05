52fb67f0d502a5012bc0ffecf6f6ddec925cbf84
Vincent T <vtis.tsangvincent15@gmail.com>
2022-07-06 00:59:29 +0800

    v1.6.0 Update

--- main.py
+++ main.py
@@ -951,12 +951,13 @@ async def game_shutdown(d: dict, guild: Guild, winner: Union[Member, str] = None
                                                     color=discord.Color.red())
                     message.set_image(url=winner.display_avatar.url)
 
-                    tasks.append(asyncio.create_task(discord.utils.get(guild.text_channels, name=sub(r'[^\w -]', '',
+                    channel = discord.utils.get(guild.text_channels, name=sub(r'[^\w -]', '',
                                                                                                      guild.get_member(
                                                                                                          int(key)).name.lower().replace(
                                                                                                          ' ',
-                                                                                                         '-')) + '-uno-channel').send(
-                        embed=message)))
+                                                                                                         '-')) + '-uno-channel')
+                    if channel:
+                        tasks.append(asyncio.create_task(channel.send(embed=message)))
 
                     if field:
                         name = guild.get_member(int(key)).name
